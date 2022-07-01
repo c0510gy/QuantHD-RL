2f5c81e1615da5d7580445ba24a41d862030b8fb
Sang-geon Yun <45253879+c0510gy@users.noreply.github.com>
2022-07-01 17:03:29 +0900

    implement test runner

--- Quant_sim.py
+++ Quant_sim.py
@@ -3,7 +3,118 @@ import copy
 import numpy as np
 import torch
 from time import time
+from tqdm import tqdm
 from QHD_QuantModel import QHD_Model
+import json
+from copy import deepcopy
+
+
+def save_as_json(filename, data):
+
+    with open(filename, 'w', encoding='utf8') as f:
+        json.dump(data, f)
+
+
+def gen_prob_table(prob, bits):
+
+    table = [[prob * 100. / 2, prob * 100. / 2] for _ in range(2**bits)]
+    table[0] = [prob * 100., 0.]
+    table[-1] = [0., prob * 100.]
+
+    return table
+
+
+def run(env, bits, dimension, prob, noise_on_inference_only, device):
+
+    history = []
+
+    prob_table = gen_prob_table(prob, bits)
+
+    env = env.unwrapped
+    ts = 5 
+    tau = 1 #update every 1 episode
+    tau_step = 100 #update every 10 steps
+
+    if task == 'cartpole':
+        epsilon = 0.2
+        epsilon_decay = 0.99
+        reward_decay = 0.9
+        EPISODES = 201
+
+    minimum_epsilon = 0.01
+    n_actions = env.action_space.n
+    n_obs = env.observation_space.shape[0]
+    model = QHD_Model(dimension, n_actions, n_obs, epsilon, epsilon_decay,
+                    minimum_epsilon, reward_decay, train_sample_size=ts, lr=0.05, device=device, bits=bits) #lr=0.05
+    copied_model = deepcopy(model)
+    #with open(filename,'a') as f:
+    #    f.write("Episode,Steps,Reward,Runtime\n")
+
+    total_runtime = 0
+    total_step = 0
+    for episode in tqdm(range(EPISODES)):
+        start = time()
+        rewards_sum = 0
+        obs = env.reset()
+        model.n_steps = 0
+
+        while True:
+            #env.render()
+            if noise_on_inference_only:
+                copied_model = deepcopy(model)
+                copied_env = deepcopy(env)
+            
+            action = int(model.act(obs))
+            new_obs, reward, done, info = env.step(action)
+            if task == 'cartpole':
+                if done:
+                    reward = -5
+            model.store_transition(obs, action, reward, new_obs, done)
+            
+            model.feedback()
+
+            if noise_on_inference_only:
+                copied_model.random_bit_flip_by_prob(prob_table)
+
+                action = int(copied_model.act(obs))
+                new_obs, reward, done, info = copied_env.step(action)
+                rewards_sum += reward
+            else:
+                rewards_sum += reward
+                model.random_bit_flip_by_prob(prob_table)
+
+            total_step += 1
+            if task == 'cartpole':
+                if total_step % tau_step == 0:
+                    model.delay_model = copy.deepcopy(model.model)
+
+            if rewards_sum > 1000:
+                done = True
+
+            # if done:
+            #     if task == 'cartpole':
+            #         if episode % tau == 0: # 5
+            #             model.delay_model = copy.deepcopy(model.model)
+
+            if done:
+                end = time()
+                total_runtime += end - start
+                #print('Episode: ', episode)
+                #print('Episode Rewards: ', rewards_sum)
+
+                history.append(rewards_sum)
+                #print('Total Runtime: ', total_runtime)
+                #with open(filename,'a') as f:
+                #    f.write(str(episode)+','+str(total_step)+','+str(rewards_sum)+','+str(total_runtime)+'\n')
+                break
+
+            model.n_steps += 1
+            obs = new_obs
+
+        model.epsilon = max(model.epsilon * model.epsilon_decay, 
+                                model.minimum_epsilon)
+    
+    return history
 
 #device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 device = 'cpu'
@@ -12,74 +123,16 @@ tasks = ['cartpole']
 for task in tasks:
     if task == 'cartpole':
         env = gym.make('CartPole-v0')
-        filenames = ['hdc_cartpole_results.csv']
+    
+    dims = [10000]
+    probs = [0., 0.20, 0.50, 0.90]
 
-    env = env.unwrapped
-    dimension = 10000
-    ts = 5 
-    tau = 1 #update every 1 episode
-    tau_step = 100 #update every 10 steps
+    for dimension in dims:
+        for prob in probs:
 
-    for filename in filenames:
-        if task == 'cartpole':
-            epsilon = 0.2
-            epsilon_decay = 0.99
-            reward_decay = 0.9
-            EPISODES = 201
+            filename = f'./01/qhd_{task}_{dimension}_{prob}_results.json'
+            print(filename)
 
-        minimum_epsilon = 0.01
-        n_actions = env.action_space.n
-        n_obs = env.observation_space.shape[0]
-        model = QHD_Model(dimension, n_actions, n_obs, epsilon, epsilon_decay,
-                        minimum_epsilon, reward_decay, train_sample_size=ts, lr=0.05, device=device, bits=3) #lr=0.05
-
-        #with open(filename,'a') as f:
-        #    f.write("Episode,Steps,Reward,Runtime\n")
-
-        total_runtime = 0
-        total_step = 0
-        for episode in range(EPISODES):
-            start = time()
-            rewards_sum = 0
-            obs = env.reset()
-            model.n_steps = 0
-
-            while True:
-                #env.render()
-                action = int(model.act(obs))
-                new_obs, reward, done, info = env.step(action)
-                if task == 'cartpole':
-                    if done:
-                        reward = -5
-                model.store_transition(obs, action, reward, new_obs, done)
-                rewards_sum += reward
-                model.feedback()
-
-                total_step += 1
-                if task == 'cartpole':
-                    if total_step % tau_step == 0:
-                        model.delay_model = copy.deepcopy(model.model)
-
-                if rewards_sum > 1000:
-                    done = True
-
-                # if done:
-                #     if task == 'cartpole':
-                #         if episode % tau == 0: # 5
-                #             model.delay_model = copy.deepcopy(model.model)
-
-                if done:
-                    end = time()
-                    total_runtime += end - start
-                    print('Episode: ', episode)
-                    print('Episode Rewards: ', rewards_sum)
-                    #print('Total Runtime: ', total_runtime)
-                    #with open(filename,'a') as f:
-                    #    f.write(str(episode)+','+str(total_step)+','+str(rewards_sum)+','+str(total_runtime)+'\n')
-                    break
-
-                model.n_steps += 1
-                obs = new_obs
-
-            model.epsilon = max(model.epsilon * model.epsilon_decay, 
-                                    model.minimum_epsilon)
+            history = run(env, 3, dimension, prob, True, device)
+            
+            save_as_json(filename, history)
---
+++ origin/QHD_QuantModel.py
@@ -0,0 +1,115 @@
+import numpy as np
+import copy
+import random
+import torch
+import math
+from tqdm import tqdm
+
+class QHD_Model(object):
+    def __init__(self,
+                dimension=10000,
+                n_actions=2,
+                n_obs=4,
+                epsilon=1.0,
+                epsilon_decay=0.995,
+                minimum_epsilon=0.01,
+                reward_decay=0.9,
+                lr = 0.005,
+                train_sample_size = 5,
+                device = 'cpu',
+                q_levels = 1001 ###(15+5)*50 + 1
+                ):
+        self.D = dimension
+        self.n_actions = n_actions
+        self.n_obs = n_obs
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.minimum_epsilon = minimum_epsilon
+        self.reward_decay = reward_decay
+        self.lr = lr
+        self.ts = train_sample_size
+        self.device = device
+        
+        self.logs = [] # temp log for current episode
+
+        self.classes = q_levels
+        self.model = torch.zeros(self.n_actions, self.classes, self.D).to(device)
+        self.basis = torch.randn(self.D, self.n_obs).to(device)
+        self.base = torch.empty(self.D).uniform_(0.0, 2*math.pi).to(device)
+        self.delay_model = copy.deepcopy(self.model)
+        
+        self.model_update_counter = 0
+    
+    def idx2value(self, index):
+        value = -5 + 0.02*index.item()
+        return value
+    
+    def value2idx(self, value):
+        index = int( (np.clip(value,-5,15) + 5) / 0.02)
+        return index
+
+    def encode(self, x):
+        if len(x.shape) == 1:
+            x = x[None]
+        n = x.size(0)
+        bsize = math.ceil(0.01*n)
+        h = torch.empty(n, self.D, device=x.device, dtype=x.dtype)
+        temp = torch.empty(bsize, self.D, device=x.device, dtype=x.dtype)
+        for i in range(0, n, bsize):
+            torch.matmul(x[i:i+bsize], self.basis.T, out=temp)
+            torch.add(temp, self.base, out=h[i:i+bsize])
+            h[i:i+bsize].cos_().mul_(temp.sin_())
+        return h
+    
+    def cos_cdist(self, x1, x2, eps=1e-8):
+        eps = torch.tensor(eps, device=x1.device)
+        norms1 = x1.norm(dim=1).unsqueeze_(1).max(eps)
+        norms2 = x2.norm(dim=1).unsqueeze_(0).max(eps)
+        cdist = x1 @ x2.T
+        cdist.div_(norms1).div_(norms2)
+        return cdist
+
+    def store_transition(self, s, a, r, n_s, done):
+        self.logs.append((s,a,r,n_s,done))
+        if len(self.logs) > 102400:
+            self.logs.pop(0)
+
+    def act(self, obs):
+        if (random.random() <= self.epsilon):
+            action = random.randint(0, self.n_actions-1)
+        else:
+            obs = self.encode(torch.FloatTensor(obs).to(self.device))
+            q_value = []
+            for a in range(self.n_actions):
+                q_idx = self.cos_cdist(obs, self.model[a]).argmax(1)
+                q_value.append(self.idx2value(q_idx))
+            action = np.argmax(q_value)
+        return action
+
+    def feedback(self):
+
+        if len(self.logs) < self.ts:
+            logs = self.logs
+        else:
+            logs = random.sample(self.logs, self.ts)
+        #for i in range(5):    
+        for log in logs:
+            (obs, action, reward, next_obs, done) = log
+            obs = torch.FloatTensor(obs).to(self.device)
+            next_obs = torch.FloatTensor(next_obs).to(self.device)
+            obs_ = self.encode(torch.FloatTensor(obs).to(self.device))
+            next_obs_ = self.encode(torch.FloatTensor(next_obs).to(self.device))
+            scores = self.cos_cdist(obs_, self.model[action])
+            y_pred = scores.argmax(1) ## index of predicted q-value
+            q_list = []
+            for a in range(self.n_actions):
+                q_list.append(self.idx2value(self.cos_cdist(next_obs_, self.model[a]).argmax(1)))
+            y_true = torch.LongTensor([self.value2idx(reward + (1-float(done))*self.reward_decay*max(q_list))]) ## index of true q-value
+            
+            #print(self.idx2value(y_pred), reward + (1-float(done))*self.reward_decay*max(q_list), action)
+            
+            ############ Update the Classification Model #############
+            if not torch.equal(y_pred, y_true):
+                self.model[action][y_pred] -= self.lr * (1.0 - torch.max(scores)) * obs_
+                self.model[action][y_true] += self.lr * (1.0 - torch.max(scores)) * obs_
+                
